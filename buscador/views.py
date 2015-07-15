2efb6cc1f895130a9194aa5594670f173a2ae159
reco <re.gunner92@gmail.com>
2015-07-15 01:05:18 -0500

    ajustes

--- buscador/views.py
+++ buscador/views.py
@@ -4,6 +4,11 @@ from django.template import RequestContext, loader
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
 import pydocumentdb.documents as documents
 import pydocumentdb.document_client as document_client
 
@@ -18,9 +23,9 @@ host = 'https://topicosbd.documents.azure.com:443/'
 
 client = document_client.DocumentClient(host,{'masterKey': masterKey})
 
-datab = list(client.ReadDatabases({'id' : 'probando'}))
+datab = list(client.ReadDatabases({'id' : 'internacional'}))
 
-collections = list(client.ReadCollections(datab[0]['_self']))
+collections = list(client.ReadCollections(datab[1]['_self']))
 
 frase_busq = ""
 paginator = ""
@@ -53,10 +58,48 @@ def consultar(request):
 			documentos = list(client.QueryDocuments(
 		    		collections[0]['_self'],
 					{
-		            	'query': 'SELECT * FROM probando i',
+		            	'query': 'SELECT * FROM internacional i',
 					}))
 
+			print len(documentos)
+
+			array = []
+			array.append(frase_busq)
+			for i in documentos:
+				palabra = i['titulo']
+
+				tags = ''
+				for t in i['tag']:
+					tags += ' ' + t
+
+				palabra = palabra + tags
+				array.append(palabra)
+
+
+
+			tfidf_vectorizer = TfidfVectorizer()
+			tfidf_matrix_train = tfidf_vectorizer.fit_transform(array)  #finds the tfidf score with normalization
+			resultado = cosine_similarity(tfidf_matrix_train[0:1], tfidf_matrix_train)  #here the first element of tfidf_matrix_train is matched with other three elements
+			ntotal = len(resultado[0])
+
+			#print resultado
+
+			i = 1
+			grupo = {}
+			while i < ntotal:
+				if resultado[0][i] > 0:
+					grupo[ resultado[0][i] ] = documentos[i-1]
+				i = i + 1
+
+			listado = grupo.items()
+			listado.sort(reverse=True)
+			documentos = []
+			for par in listado:
+				documentos.append(par[1])
+
 			nro_resultados = len(documentos)
+
+			print nro_resultados
 
 			paginator = Paginator(documentos, 8) 
 
@@ -119,8 +162,19 @@ def consultarlink(request,palabra):
 			documentos = list(client.QueryDocuments(
 		    		collections[0]['_self'],
 					{
-		            	'query': 'SELECT * FROM probando i',
+		            	'query': 'SELECT * FROM internacional i',
 					}))
+
+			array = []
+			array.append(frase_busq)
+			for i in documentos:
+				array.append(i['titulo'])
+
+			print array
+
+			tfidf_vectorizer = TfidfVectorizer()
+			tfidf_matrix_train = tfidf_vectorizer.fit_transform(array)  #finds the tfidf score with normalization
+			print "cosine scores ==> ",cosine_similarity(tfidf_matrix_train[0:1], tfidf_matrix_train)  #here the first element of tfidf_matrix_train is matched with other three elements
 
 			nro_resultados = len(documentos)
 
